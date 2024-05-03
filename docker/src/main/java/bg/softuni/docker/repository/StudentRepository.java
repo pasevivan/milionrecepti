0877bbab132e27181c1a7bab4d96778eeb1c4555
Ivan Pasev <pasevivan@gmail.com>
2024-05-03 15:25:48 +0300

    Delete docker directory

--- docker/src/main/java/bg/softuni/docker/repository/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package bg.softuni.docker.repository;
-
-import bg.softuni.docker.model.StudentEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface StudentRepository extends JpaRepository<StudentEntity, Long> {
-}
