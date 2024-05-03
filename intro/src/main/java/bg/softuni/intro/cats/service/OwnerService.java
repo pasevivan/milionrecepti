5122591be24acb9683376b6d70b9ce69adf5fdde
Ivan Pasev <pasevivan@gmail.com>
2024-05-03 15:26:25 +0300

    Delete intro directory

--- intro/src/main/java/bg/softuni/intro/cats/service/OwnerService.java
+++
@@ -1,9 +0,0 @@
-package bg.softuni.intro.cats.service;
-
-import bg.softuni.intro.cats.model.dto.CreateOwnerDTO;
-
-public interface OwnerService {
-
-  void createOwner(CreateOwnerDTO createOwnerDTO);
-
-}
--- intro/src/main/java/bg/softuni/intro/ioc/Cat.java
+++
@@ -1,9 +0,0 @@
-package bg.softuni.intro.ioc;
-
-public class Cat implements Animal{
-
-  @Override
-  public void makeNoise() {
-    System.out.println("mew mew");
-  }
-}
